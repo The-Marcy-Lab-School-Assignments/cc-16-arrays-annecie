2866e3507c826c100c729a0707eb132b399132b8
annecie <ajbexcelsior@gmail.com>
2021-11-16 15:41:17 +0000

    completed

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 //write your code here
+const replaceWithYerr = (array, start = 0, num= array.length-1 )=>{
+    const replaced = [];
+    for (let i = 0; i< array.length; i++){
+        if (i > start && i < start+num){
+            replaced.push("yerr")
+        }else{
+            replaced.push(array[i])
+        }
+    }
+    return replaced
+}
+const months= ['Jan', 'February', 'March', 'April', 'May', 'June'];
+
+console.log(replaceWithYerr(months, 1,3))
